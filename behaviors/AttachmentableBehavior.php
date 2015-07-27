f4decb0c61509c4054f32f6ddd1a228cb6619e17
mitrii <p.mitrii@gmail.com>
2015-07-27 17:20:00 +0600

    method for get main photo

--- behaviors/AttachmentableBehavior.php
+++ behaviors/AttachmentableBehavior.php
@@ -7,6 +7,15 @@ use mitrii\attachments\models\Attachment;
 
 class AttachmentableBehavior extends \yii\base\Behavior
 {
+    public function getMain_photo()
+    {
+        $object = get_class($this->owner);
+        $object_id = $this->owner->id;
+
+        $attachment = Attachment::find()->where(['object' => $object, 'object_id' => $object_id])->orderBy('id')->one();
+        return (empty($attachment)) ? null : $attachment->hash;
+    }
+
     public function getAttachments()
     {
         $object = get_class($this->owner);
@@ -15,9 +24,9 @@ class AttachmentableBehavior extends \yii\base\Behavior
         return Attachment::findByObject($object, $object_id);
     }
 
-    public function attachAttachments()
+    public function attachAttachments($param_name = 'attachments')
     {
-        $attachments_data = json_decode(Yii::$app->getRequest()->post('attachments', '[]'));
+        $attachments_data = json_decode(Yii::$app->getRequest()->post($param_name, '[]'));
         $attachments_data = (empty($attachments_data)) ? [] : $attachments_data;
 
 
