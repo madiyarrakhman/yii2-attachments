4be13ffccf3215ff9a8bdc1a63a04f6ba15e76a7
mitrii <p.mitrii@gmail.com>
2016-03-18 13:05:15 +0600

    is_downloadable migration

---
+++ migrations/m160318_065826_attachment_downloadable.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\db\Migration;
+
+class m160318_065826_attachment_downloadable extends Migration
+{
+    public function up()
+    {
+        $this->addColumn('attachment', 'is_downloadable', $this->boolean()->defaultValue(false));
+    }
+
+    public function down()
+    {
+        $this->dropColumn('attachment', 'is_downloadable');
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
