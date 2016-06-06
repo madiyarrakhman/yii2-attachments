33f46ef179accb43f7939636444c22c6fb5d12a1
Dmitriy Pushkaryov <p.mitrii@gmail.com>
2016-06-06 13:12:36 +0000

    fix always default resize mode

--- helpers/Image.php
+++ helpers/Image.php
@@ -9,6 +9,6 @@ class Image
 {
     static public function url($hash, $width, $height, $mode = null)
     {
-        return Yii::$app->getModule('attachment')->get('render')->getImageUrl($hash, $width, $height, $mode = null);
+        return Yii::$app->getModule('attachment')->get('render')->getImageUrl($hash, $width, $height, $mode);
     }
 } 
