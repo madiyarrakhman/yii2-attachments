b1a726ca16a9bbcb1ddd75965edbbeb3c2cd4e40
mitrii <p.mitrii@gmail.com>
2019-06-24 15:50:28 +0600

    fixes

---
+++ actions/LegacyAddAction.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Poject: kapital2
+ * User: mitrii
+ * Date: 2019-06-21
+ * Time: 14:52
+ * Original File Name: LegacyAddAction.php
+ */
+
+namespace musan\attachments\actions;
+
+
+class LegacyAddAction
+{
+
+}
---
+++ actions/UploadAction.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Poject: kapital2
+ * User: mitrii
+ * Date: 2019-06-21
+ * Time: 14:18
+ * Original File Name: UploadAction.php
+ */
+
+namespace musan\attachments\actions;
+
+
+class UploadAction
+{
+
+}
